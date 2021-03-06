055f61a0d7f80256f36e91c0fe0d64789c3e77da
Tylerflx <tylerflx@Tylerflxs-MacBook-Pro.local>
2021-03-06 02:20:52 -0500

    Simple node.js example

---
+++ index.js
@@ -0,0 +1,9 @@
+const superheroes = require('superheroes');
+
+const supervillains = require('supervillains');
+
+var superHero = superheroes.random();
+
+var superVil = supervillains.random();
+
+console.log("This match is: "+ superHero + " vs " + superVil);
